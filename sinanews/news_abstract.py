62ab6e6f274a4a123dcf0f9600c0875916ce781c
day9011 <329723954@qq.com>
2016-04-02 14:38:18 +0800

    json function

--- sinanews/news_abstract.py
+++ sinanews/news_abstract.py
@@ -3,6 +3,7 @@
 __author__ = 'dinghanyu'
 
 from tornado.web import RequestHandler
+from news_model import data_to_json
 import tornado
 import json
 import mydb
@@ -13,21 +14,7 @@ class news_abstract(RequestHandler):
         db = mydb.Mydb()
  #       news = db.query_news('SELECT news_attribute.*, news_contents.news_abstract FROM news_attribute, news_contents WHERE news_attribute.news_id = news_contents.news_id')
         news = db.query_news()
-        jsonstr = '{'
-        jsonstr += '"pre": true,'
-        jsonstr += '"current_cursor": 2,'
-        jsonstr += '"news":['
-        for item in news:
-            jsonstr += json.dumps(item) + ','
-        jsonstr = jsonstr[:-1]
-        jsonstr += ']}'
-        self.write(jsonstr)
+        returndata = data_to_json(True, news)
+        self.write(returndata)
         self.finish()
 
-    def post(self):
-        post_data = {}
-        for key in self.request.arguments:
-            post_data[key] = self.get_argument(key)
-        print post_data
-        self.write('test for post')
-        self.finish()
--- sinanews/news_model.py
+++ sinanews/news_model.py
@@ -1,6 +1,7 @@
 __author__ = 'dinghanyu'
 #!/usr/lib/python
 #coding=utf-8
+import json
 
 class newsData:
     def __init__(self):
@@ -16,3 +17,14 @@ class newsData:
             "abstract" : "",
             "image" : "",
         }
+
+def data_to_json(pre, news):
+    jsonstr = '{'
+    jsonstr += '"pre": true,'
+    jsonstr += '"current_cursor": 2,'
+    jsonstr += '"news":['
+    for item in news:
+        jsonstr += json.dumps(item) + ','
+    jsonstr = jsonstr[:-1]
+    jsonstr += ']}'
+    return jsonstr
